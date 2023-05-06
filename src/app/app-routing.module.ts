96b1a8f9c86a4c697507df238fe47b703db2b30c
Rafael Santos Neves <r4fael.neves2004@gmail.com>
2023-05-05 22:39:09 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SobreMimComponent } from './componentes/sobre-mim/sobre-mim.component';
+import { TecnologiasComponent } from './componentes/tecnologias/tecnologias.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SobreMimComponent },
+  { path: 'tecnologias', component: TecnologiasComponent },
+
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SobreMimComponent } from './componentes/sobre-mim/sobre-mim.component';
+import { TecnologiasComponent } from './componentes/tecnologias/tecnologias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreMimComponent,
+    TecnologiasComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
